50e48a600e6522dccf9ef7082a2752962f8877cf
Shubham Sharma <shubh133@gmail.com>
2015-03-19 00:35:11 +0530

    Added Twinkling Triangle animation to Anti-Hexagon

--- js/scroll.js
+++ js/scroll.js
@@ -1,3 +1,27 @@
+//For Performance
+var triangleTwinkleAnimationDone = false;
+
+function randomTriangles (min, max) {
+    var randomArray = [];
+    while (randomArray.length < max) {
+        var randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
+        var randomNumberExists = false;
+
+        for (var i = 0; i < randomArray.length; ++i) {
+            if (randomNumber == randomArray[i]) {
+                randomNumberExists = true;
+                break;
+            } 
+        }
+
+        if (!randomNumberExists) {
+            randomArray.push(randomNumber);
+        }
+    }
+    return randomArray;
+};
+
+
 $(window).scroll(function() { 
     var scrollPos = $(this).scrollTop();
 
@@ -10,5 +34,41 @@ $(window).scroll(function() {
 
         $('.banner-content-wrapper').css('top', percentScroll + 'px');
     }
+
+    //Twinkling Triangle animation for anti-hexagons
+    if (scrollPos > Math.round(offsetTop + bannerHeight / 2) &&
+        scrollPos < Math.round(offsetTop + bannerHeight * 1.5)) {
+
+        if (!triangleTwinkleAnimationDone) {
+            var hexagons = $('.hexagon');
+            var randomArray = randomTriangles(1, hexagons.length);
+
+            var i = 0;
+            var animationDelay = 50;
+            function animateTriangle() {
+                $(hexagons[randomArray[i] - 1]).addClass('hide-triangles');
+                i++;
+                if (i < randomArray.length) {
+                    setTimeout(animateTriangle, animationDelay);
+                }
+            };
+            setTimeout(animateTriangle, animationDelay);
+
+            triangleTwinkleAnimationDone = true;
+        }
+
+    }
+    else {
+
+        if (triangleTwinkleAnimationDone) {
+            console.log('class removed');
+            $('.hexagon').removeClass('hide-triangles');
+        }
+        triangleTwinkleAnimationDone = false;
+
+    }
+
+
+
 
 });
