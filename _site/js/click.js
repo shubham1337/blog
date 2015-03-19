79752cbc10264a307cd59fde94a74e2f04465b62
Shubham Sharma <shubh133@gmail.com>
2015-03-19 16:50:41 +0530

    Better organizing

--- _site/js/click.js
+++ _site/js/click.js
@@ -1,18 +1,20 @@
-$('.section-heading-wrapper').click(function() { 
-    var sectionOffset = $(this).offset().top;
-    sectionOffset += 15;
+function clickAnimations () {
+    $('.section-heading-wrapper').click(function() { 
+        var sectionOffset = $(this).offset().top;
+        sectionOffset += 15;
 
-    var scrollPosition = $(window).scrollTop();
+        var scrollPosition = $(window).scrollTop();
 
-    $('html,body').animate({scrollTop: sectionOffset}, 1000);
-    //in-house animation function, too slow, no easing or smoothness
-    /*function smoothScroll() {
-        $(window).scrollTop(scrollPosition);
+        $('html,body').animate({scrollTop: sectionOffset}, 1000);
+        //custom animation function, too slow, no easing or smoothness
+        /*function smoothScroll() {
+            $(window).scrollTop(scrollPosition);
 
-        if (sectionOffset > scrollPosition) {
-            scrollPosition += 5;
-            setTimeout(smoothScroll, 1);
+            if (sectionOffset > scrollPosition) {
+                scrollPosition += 5;
+                setTimeout(smoothScroll, 1);
+            }
         }
-    }
-    setTimeout(smoothScroll, 1);*/
-});
+        setTimeout(smoothScroll, 1);*/
+    });
+}
--- _site/js/onload.js
+++ _site/js/onload.js
@@ -1,6 +1,10 @@
-//For Performance reasons
 $(document).ready(function() { 
+    //For Performance
     bannerOffset = $('.banner').offset();
     bannerHeight = $('.banner').height();
     bannerWidth = $('.banner').width();
+
+    parallaxAnimation();
+    scrollingAnimations();
+    clickAnimations();
 });
